b85aa6fbc3d4a352409199a919420c6e10f77d84
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-07-24 21:31:47 +0300

    Just about complete challenge 2

---
+++ expenses-chart-component/src/App.tsx
@@ -0,0 +1,91 @@
+const data = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+];
+function App() {
+  const renderChart = (chartData: typeof data) => {
+    const maxAmount = Math.max(...chartData.map((item) => item.amount));
+
+    const renderBar = (day: { day: string; amount: number }, index: number) => {
+      const barPercentage = (day.amount / maxAmount) * 100;
+      const barHeight = Math.round((barPercentage / 100) * 144);
+      const baseStyle = `rounded w-10 md:w-12`;
+      const bgColor = day.amount === maxAmount ? "bg-cyan" : "bg-softRed";
+      const style = `${bgColor} ${baseStyle} hover:opacity-50`;
+      // invisible group-hover:visible
+      return (
+        <div className="flex flex-col justify-end group" key={index}>
+          <div className="bg-black opacity-75 text-center text-white mb-1 md:mb-2 rounded">
+            ${day.amount}
+          </div>
+          <div className="flex flex-col mx-2">
+            <div style={{ height: barHeight }} className={style}></div>
+            <span className="self-center text-gray-400">{day.day}</span>
+          </div>
+        </div>
+      );
+    };
+    return chartData.map(renderBar);
+  };
+  return (
+    <div className="flex flex-col h-screen justify-center m-auto max-w-lg">
+      <div className="flex bg-softRed text-white justify-between p-5 rounded-3xl shadow-md">
+        <div className="flex flex-col">
+          <span className="text-lg">My balance</span>
+          <span className="text-3xl pt-2">$921.48</span>
+        </div>
+        <div className="flex z-10 -space-x-5">
+          <div className="self-center h-12 w-12 border-2 rounded-full z-10 border-white"></div>
+          <div className="self-center h-12 w-12 border ml-10 -z-1 rounded-full border-black bg-black inline-block"></div>
+        </div>
+      </div>
+      <div className="flex flex-col bg-white rounded-3xl mt-5 shadow-md">
+        <span className="text-3xl font-semibold p-5">
+          Spending - Last 7 Days
+        </span>
+        <div className="flex flex-row space-x px-8 py-10">
+          {renderChart(data)}
+        </div>
+        <div className="border max-w-[90%]"></div>
+        <div className="flex justify-between flex-row px-8 py-10">
+          <div className="flex flex-col">
+            <span className="text-gray-400">Total this month</span>
+            <span className="text-5xl font-bold">$478.33</span>
+          </div>
+          <div className="flex flex-col justify-center">
+            <span className="self-end font-bold">+2.4%</span>
+            <span className="text-gray-400">from last month</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ expenses-chart-component/tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        softRed: "hsl(10, 79%, 65%)",
+        cyan: "hsl(186, 34%, 60%)",
+        darkBrown: "hsl(25, 47%, 15%)",
+        mediumBrown: "hsl(28, 10%, 53%)",
+        cream: "hsl(27, 66%, 92%)",
+        veryPaleOrange: "hsl(33, 100%, 98%)",
+      },
+    },
+  },
+  plugins: [],
+  variants: {
+    extend: {
+      display: ["group-hover"],
+    },
+  },
+};
