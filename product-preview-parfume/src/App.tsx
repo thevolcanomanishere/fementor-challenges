6607b2d0feebb5f911feead34c4e2240d5a5fcd5
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-08-08 17:59:44 +0300

    Add blue

--- product-preview-parfume/src/App.tsx
+++ product-preview-parfume/src/App.tsx
@@ -61,6 +61,7 @@ function App() {
         <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
         <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
         <div className="absolute -bottom-8 right-20 w-72 h-72 bg-green-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+        <div className="absolute -bottom-1/2 right-1/2 top-1/2 left-1/2  w-72 h-72 bg-blue-600 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
       </div>
     </div>
   );
